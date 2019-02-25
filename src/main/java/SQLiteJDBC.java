a9c7586bd9c07eeb87dc59d109f12b2832efdac0
bartek <bar.gem@hotmail.com>
2019-02-25 10:56:55 +0100

    noen changes

--- src/main/java/SQLiteJDBC.java
+++ src/main/java/SQLiteJDBC.java
@@ -7,7 +7,7 @@ public class SQLiteJDBC {
     }
 
     public static void createNewDatabase( String fileName ) {
-        String url = "jdbc:sqlite:C:\\Users\\saomoe\\Documents\\javaAssignments\\Week03\\src\\main\\resources\\" + fileName;
+        String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
 
         try (Connection conn = DriverManager.getConnection(url)) {
             if (conn != null) {
@@ -24,7 +24,7 @@ public class SQLiteJDBC {
     public static void connectDB() {
         Connection conn = null;
         try {
-            String url = "jdbc:sqlite::resources:task17.sqlite";
+            String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
             conn = DriverManager.getConnection(url);
             System.out.println("Connected");
         } catch (SQLException ex) {
@@ -40,35 +40,22 @@ public class SQLiteJDBC {
         }
     }
 
-    public static void dropTable() {
-        String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
-        String sql = "DROP TABLE IF EXISTS Person";
 
-        try (Connection conn = DriverManager.getConnection(url);
-             Statement stmt = conn.createStatement()) {
-            // create a new table
-            stmt.execute(sql);
-            System.out.println("success");
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-
-    }
 
     public static void createNewTable() {
         // SQLite connection string
         String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
 
         // SQL statement for creating a new table
-        String sql = "CREATE TABLE IF NOT EXISTS Person (\n"
+        String sql = "CREATE TABLE IF NOT EXISTS person (\n"
                 + "	id integer PRIMARY KEY,\n"
                 + "	firstName text NOT NULL,\n"
                 + "	lastName text NOT NULL,\n"
-                + " birth text NOT NULL, \n"
-                + " adress text NOT NULL, \n"
-                + " relatives text NOT NULL, \n"
-                + " personalEmail text NOT NULL, \n"
-                + " workEmail text NOT NULL \n"
+                + "	birth text NOT NULL,\n"
+                + "	address text NOT NULL,\n"
+                + "	relatives text NULL,\n"
+                + "	personalMail text NULL,\n"
+                + "	workMail text NULL\n"
                 + ");";
 
         try (Connection conn = DriverManager.getConnection(url);
