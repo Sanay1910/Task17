342998545715a4dd25c6ecc045b72190088e3a73
Sanay <sanay1910@gmail.com>
2019-02-25 10:51:26 +0100

    haha

--- src/main/java/SQLiteJDBC.java
+++ src/main/java/SQLiteJDBC.java
@@ -3,7 +3,7 @@ import java.sql.*;
 public class SQLiteJDBC {
 
     public static void main(String [] args) {
-        createNewDatabase("task17.sqlite");
+        createNewTable();
     }
 
     public static void createNewDatabase( String fileName ) {
@@ -24,7 +24,7 @@ public class SQLiteJDBC {
     public static void connectDB() {
         Connection conn = null;
         try {
-            String url = "";
+            String url = "jdbc:sqlite::resources:task17.sqlite";
             conn = DriverManager.getConnection(url);
             System.out.println("Connected");
         } catch (SQLException ex) {
@@ -39,6 +39,49 @@ public class SQLiteJDBC {
             }
         }
     }
+
+    public static void dropTable() {
+        String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
+        String sql = "DROP TABLE IF EXISTS Person";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+            System.out.println("success");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+    }
+
+    public static void createNewTable() {
+        // SQLite connection string
+        String url = "jdbc:sqlite:src/main/resources/task17.sqlite";
+
+        // SQL statement for creating a new table
+        String sql = "CREATE TABLE IF NOT EXISTS Person (\n"
+                + "	id integer PRIMARY KEY,\n"
+                + "	firstName text NOT NULL,\n"
+                + "	lastName text NOT NULL,\n"
+                + " birth text NOT NULL, \n"
+                + " adress text NOT NULL, \n"
+                + " relatives text NOT NULL, \n"
+                + " personalEmail text NOT NULL, \n"
+                + " workEmail text NOT NULL \n"
+                + ");";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            // create a new table
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+
 
 
 
