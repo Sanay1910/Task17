1bc4163f7348dbd9bde220a5f6d1d6fc488c95b3
Sanay <sanay1910@gmail.com>
2019-02-25 10:22:03 +0100

    db created

--- src/main/java/SQLiteJDBC.java
+++ src/main/java/SQLiteJDBC.java
@@ -1,8 +1,24 @@
 import java.sql.*;
 
 public class SQLiteJDBC {
-    public static void main( String [] args) {
 
+    public static void main(String [] args) {
+        createNewDatabase("task17.sqlite");
+    }
+
+    public static void createNewDatabase( String fileName ) {
+        String url = "jdbc:sqlite:C:\\Users\\saomoe\\Documents\\javaAssignments\\Week03\\src\\main\\resources\\" + fileName;
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                DatabaseMetaData meta = conn.getMetaData();
+                System.out.println("The driver name is " + meta.getDriverName());
+                System.out.println("A new database has been created.");
+            }
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
     }
 
     public static void connectDB() {
@@ -23,6 +39,8 @@ public class SQLiteJDBC {
             }
         }
     }
+
+
 
 
 }
